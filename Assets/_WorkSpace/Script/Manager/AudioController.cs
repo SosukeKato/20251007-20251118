f1be1258b8a896f438793c5854714d8761f29763
SosukeKato <dokindokinpiano@gmail.com>
2025-10-07 16:58:23 +0900

    AudioManagerをSingletonに設定

--- Assets/_WorkSpace/Script/Manager/AudioController.cs
+++ Assets/_WorkSpace/Script/Manager/AudioController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    static AudioController instance { get; set; }
+
     void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
