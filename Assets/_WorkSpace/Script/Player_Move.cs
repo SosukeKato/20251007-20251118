efc4b380983b67ba3d47b2f3e8882b467629fa80
shuto20051014 <shuto20051014@gmail.com>
2025-10-11 11:44:14 +0900

    [add]groundタグの追加

--- Assets/_WorkSpace/Script/Player_Move.cs
+++ Assets/_WorkSpace/Script/Player_Move.cs
@@ -7,6 +7,7 @@ public class Player_Move : MonoBehaviour
     public float _time = 0;
     public bool _isgrounded = false;
     public float _jumpforce = 1;
+    [SerializeField] float _Maxjumptime =5;
     Rigidbody2D _Rb2D;
     void Start()
     {
@@ -29,10 +30,21 @@ public class Player_Move : MonoBehaviour
         }
         if (Input.GetMouseButtonUp(0))
         {
-            _Rb2D.AddForce(Vector2.up * _jumpforce * _time);
-            Debug.Log(Vector2.up * _jumpforce * _time);
-            Debug.Log("ジャンプ");
-            _isgrounded = false;
+              _Rb2D.AddForce(Vector2.up * _jumpforce * _time);
+              Debug.Log(Vector2.up * _jumpforce * _time);
+              Debug.Log("ジャンプ");
+            if (_time > _Maxjumptime)
+            {
+                _time = _Maxjumptime;
+                Debug.Log("最大火力");
+            }
+              _isgrounded = false;
+              _time = 0;
+        }
+        if (!_isgrounded)
+        {
+            float gravity = Time.deltaTime * _Rb2D.gravityScale;
+            _Rb2D.velocity += Vector2.up * gravity;
         }
        Debug.Log(_isgrounded);
     }
