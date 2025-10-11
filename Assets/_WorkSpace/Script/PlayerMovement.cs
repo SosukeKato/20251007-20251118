cb6fa6e50352e2cf32eeed29008058b2679189cd
DESKTOP-D6ASBL6\vantan <yukiyuki.game1204@icloud.com>
2025-10-11 12:31:33 +0900

    [add]テスト用のスクリプトPlayerMovement.csを追加

---
+++ Assets/_WorkSpace/Script/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerJump : MonoBehaviour
+{
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // SpaceÇ≈ÉWÉÉÉìÉv
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    // ê⁄ínîªíË
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
