ab6efd150959a024bb1e83c1ccb8aa2c77639f69
shuto20051014 <shuto20051014@gmail.com>
2025-10-13 19:40:01 +0900

    [add] ジャンプの制限の追加

--- Assets/_WorkSpace/Script/Player_Move.cs
+++ Assets/_WorkSpace/Script/Player_Move.cs
@@ -1,13 +1,12 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player_Move : MonoBehaviour
 {
     public Transform _player;
-    public float _time = 0;
-    public bool _isgrounded = false;
-    public float _jumpforce = 1;
-    [SerializeField] float _Maxjumptime =5;
+    private float _time = 0;
+    private bool _isgrounded = false;
+    public float _jumpforce = 0.01f;
+    [SerializeField] float _Maxjumptime =3;
     Rigidbody2D _Rb2D;
     void Start()
     {
@@ -26,25 +25,25 @@ public class Player_Move : MonoBehaviour
         if (Input.GetMouseButton(0)&&_isgrounded == true)
         {
             _time += Time.deltaTime;
-            Debug.Log("ジャンプ溜めてます");
+            if (_time > _Maxjumptime)
+            {
+                Debug.Log("最大");
+                _time = _Maxjumptime;
+            }
+            Debug.Log( _time +"秒ジャンプ溜めてます");
         }
         if (Input.GetMouseButtonUp(0))
         {
-              _Rb2D.AddForce(Vector2.up * _jumpforce * _time);
-              Debug.Log(Vector2.up * _jumpforce * _time);
-              Debug.Log("ジャンプ");
-            if (_time > _Maxjumptime)
-            {
-                _time = _Maxjumptime;
-                Debug.Log("最大火力");
-            }
-              _isgrounded = false;
-              _time = 0;
+                _Rb2D.AddForce(Vector2.up * _jumpforce * _time );
+            Debug.Log(Vector2.up * _jumpforce * _time);
+           // Debug.Log(Vector2.up * _jumpforce * _time);
+                _isgrounded = false;
+                _time = 0;
         }
         if (!_isgrounded)
         {
             float gravity = Time.deltaTime * _Rb2D.gravityScale;
-            _Rb2D.velocity += Vector2.up * gravity;
+            _Rb2D.velocity += Vector2.down * gravity;
         }
        Debug.Log(_isgrounded);
     }
