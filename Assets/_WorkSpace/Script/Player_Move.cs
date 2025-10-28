59d74695a6aed329380430aa84ad446b7fee6b4b
shuto20051014 <shuto20051014@gmail.com>
2025-10-28 11:49:39 +0900

    [add]playerにアニメーションを追加

--- Assets/_WorkSpace/Script/Player_Move.cs
+++ Assets/_WorkSpace/Script/Player_Move.cs
@@ -5,21 +5,27 @@ public class Player_Move : MonoBehaviour
     public Transform _player;
     private float _time = 0;
     private bool _isgrounded = false;
+    private bool _isfall = false;
     public float _jumpforce = 0.01f;
     [SerializeField] float _JumpingMoveSpeed = 10;
     [SerializeField] float _Maxjumptime =3;
     [SerializeField] float _deadzone = 1;//playerの真上の判定を取るための変数
     Rigidbody2D _Rb2D;
     private float _direction = 0;
+    Animator _anim;
     void Start()
     {
         _Rb2D = GetComponent<Rigidbody2D>();
+        _anim = GetComponentInChildren<Animator>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
             _isgrounded = true;
+            _isfall = false;
+            _anim.SetBool("isJumping", false);
+            _anim.SetBool("isfall", false);
         }
     }
     void OnCollisionExit2D(Collision2D collision)
@@ -27,6 +33,7 @@ public class Player_Move : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             _isgrounded = false;
+            _anim.SetBool("isJumping", true);
         }
     }
     void Update()
@@ -38,18 +45,27 @@ public class Player_Move : MonoBehaviour
         float dx = mousepos.x - _player.position.x;//プレイヤーとマウスの距離
         if (dx > 0)//プレイヤーが右を向いていたら
         {
-            _player.localScale = new Vector3(-1, 1, 1);
+            _player.localScale = new Vector3(1, 1, 1);
             _direction = 1;
         }
         if (dx < 0)//プレイヤーが左を向いていたら
         {
-            _player.localScale = new Vector3(1, 1, 1);
+            _player.localScale = new Vector3(-1, 1, 1);
             _direction = -1;
         }
         if (Mathf.Abs(dx) < _deadzone)//マウスがプレイヤーの真上にあったら
         {
             _direction = 0;
         }
+        if (_isgrounded)
+        {
+            _isfall = false;
+        }
+        else if (_Rb2D.velocity.y < -0.01f)
+        {
+            _isfall = true;
+        }
+        _anim.SetBool("isfall", _isfall);
         if (Input.GetMouseButton(0) && _isgrounded == true)
         {
             _time += Time.deltaTime;
